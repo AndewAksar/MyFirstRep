18ca83957fc0c9e38c10454fb5fd04062f6e7f5e
aksar <aandrew773@gmail.com>
2023-02-02 18:00:03 +0500

    init

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ def foo(a, b):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print(2, 2)
+    print(2, 4)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
